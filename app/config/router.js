0a89c8cc8830769ba209aab8181fd1cd15e2a948
Jonathan Geiger <geiger@freshbooks.com>
2014-01-04 19:15:13 -0500

    Adding links to a project view page.

--- app/config/router.js
+++ app/config/router.js
@@ -3,6 +3,7 @@
 module.exports = App.Router.map(function() {
 
   this.resource('projects');
+  this.resource('project', { path: '/projects/:project_id' });
   this.resource('clients');
   this.resource('client', {path: 'clients/:id'}, function(){
     this.route('edit')
