5812c41d6e0af2088be474ffa624fee6b974d4e5
Jonathan Geiger <geiger@freshbooks.com>
2014-01-05 14:10:42 -0500

    Handling cases with missing time entries.

--- app/controllers/ProjectController.js
+++ app/controllers/ProjectController.js
@@ -1,21 +1,12 @@
 'use strict';
 
 module.exports = App.ProjectController = Ember.ObjectController.extend({
-  managerName: function () {
-    return this.get('managedBy.name')
-  }.property('managedBy.name'),
-
-  clientName: function () {
-    return this.get('client.name')
-  }.property('client.name'),
-
+  managerName: Ember.computed.alias('managedBy.name'),
+  clientName: Ember.computed.alias('client.name'),
+  hoursBudgeted: Ember.computed.alias('hourBudget'),
   hoursLogged: function () {
     return this.get('timeEntries').reduce(function (previous, timeEntry) {
       return timeEntry.get('hours') + (previous ? previous : 0);
-    });
+    }) || 0;
   }.property('timeEntries.@each.hours'),
-
-  hoursBudgeted: function () {
-    return this.get('hourBudget');
-  }.property('hourBudget'),
 });
